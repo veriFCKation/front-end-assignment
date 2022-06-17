3b34329fa6f330efb54a73959026b6af0836b488
veriFCKation <k.shchekina@innopolis.university>
2022-06-17 18:46:15 +0300

    initial commit

---
+++ main.js
@@ -0,0 +1,11 @@
+/*const dogButt = document.getElementById("dogButton");
+
+dogButt.addEventListener('click', findDog());
+
+async function findDog(){
+    const response = await fetch("https://dog.ceo/api/breeds/image/random");
+    let data = await response.json();
+    document.getElementById('new-photo').style.display = 'block';
+    //console.log(data);
+    document.getElementById('random-dog').src = data.message;
+}*/
